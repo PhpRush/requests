d486eb8104c515f9aab993814a251526569865fd
root <root@hsb.shebao.net>
2017-10-25 18:29:13 +0800

    init

---
+++ requests/src/Demo.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiangjianyong
+ * Date: 2017/10/25
+ * Time: 18:28
+ */
+
+namespace Rush\Src;
+
+
+class Demo
+{
+
+    public function say()
+    {
+        return "Hello World!";
+    }
+}
